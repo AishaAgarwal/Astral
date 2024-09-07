d5fe096eeaceec3a715913b41fa07911b0772427
AishaAgarwal <aishaagarwal19567@gmail.com>
2024-09-07 22:35:55 +0530

    feat : update and delete goods

--- controllers/goodsController.js
+++ controllers/goodsController.js
@@ -61,4 +61,73 @@ const getItemDetails = async (req,res) => {
     }
 };
 
-module.exports = {createItem, getItemDetails};
+// update goods details 
+const updateItem = async(req,res) => {
+    const {goodsId} = req.params;
+    const {name, description, category, price} = req.body;
+
+    if(!name || !description || !category || typeof price !== 'number'){
+        return res.status(400).json({
+            message : 'invalid request data'
+        });
+    }
+
+    try{
+        const goods = await Goods.findById(goodsId);
+
+        if(!goods){
+            return res.status(404).json({
+                message : 'item not found'
+            });
+        }
+
+        goods.name = name;
+        goods.description = description;
+        goods.category = category;
+        goods.price = price;
+
+        await goods.save();
+
+        res.status(200).json({
+            goodsId: goods._id,
+            name: goods.name,
+            description: goods.description,
+            category: goods.category,
+            price: goods.price
+        });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+}
+
+// delete goods 
+const deleteItem = async (req, res) => {
+    const { goodsId } = req.params;
+
+    try {
+        const goods = await Goods.findById(goodsId);
+
+        if (!goods) {
+            return res.status(404).json({
+                message: 'Item not found'
+            });
+        }
+
+        await Goods.findByIdAndDelete(goodsId);
+
+        res.status(200).json({
+            message: 'Item deleted successfully'
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+};
+
+module.exports = {createItem, getItemDetails, updateItem, deleteItem};
--- routes/goodsRoute.js
+++ routes/goodsRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createItem, getItemDetails} = require('../controllers/goodsController');
+const {createItem, getItemDetails, updateItem, deleteItem} = require('../controllers/goodsController');
 
 const router = express.Router();
 
@@ -8,5 +8,11 @@ router.post('/create', createItem);
 
 // retrieve an item by ID 
 router.get('/:goodsId',getItemDetails);
+
+// update an item 
+router.put('/:goodsId', updateItem);
+
+//delete item 
+router.delete('/:goodsId',deleteItem);
 
 module.exports = router;
